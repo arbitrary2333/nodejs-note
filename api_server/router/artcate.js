68d627921ad81e343023a160a041a6d485777c3c
柳龙辉 <yjh15039781792@163.com>
2022-07-12 11:45:02 +0800

    文章分类管理

--- api_server/router/artcate.js
+++ api_server/router/artcate.js
@@ -2,8 +2,20 @@ const express = require('express')
 
 const router = express.Router()
 
+const expressJoi = require('@escook/express-joi')
+
+const { add_cate_schema, delete_cate_schema, get_cate_schema, update_cate_schema } = require('../schema/artcate')
+
 const articate_handler = require('../router_handler/artcate')
 
 router.get('/cates', articate_handler.getArticleCates)
+
+router.post('/addCate', expressJoi(add_cate_schema), articate_handler.addArticleCates)
+
+router.get('/deletecate/:id', expressJoi(delete_cate_schema), articate_handler.deleteCateById)
+
+router.get('/cates/:id',expressJoi(get_cate_schema) , articate_handler.getArtCateById)
+
+router.post('/updatecate', expressJoi(update_cate_schema), articate_handler.updateCateById)
 
 module.exports = router
